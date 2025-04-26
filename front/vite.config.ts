fccbed0cf532dfaf4966e6d64800e18a6f053b39
Abel Herrera <onosmx@gmail.com>
2025-04-26 00:04:39 +0000

    allowed hosts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),],
-  define: { 'process.env': {}, }
+  define: { 'process.env': {}, },
+  server: {
+    allowedHosts: ["http://f0s8848c8gkgoogo48co8ogk.167.114.30.219.sslip.io/"]
+  }
 })
