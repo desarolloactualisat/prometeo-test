5e617c5c1e4b8995edf0ea2cc5fab94a6819acbf
desarolloactualisat <desarrollo.actualisat@gmail.com>
2025-04-29 22:24:19 +0000

    u8yh8uy

--- back/app/main.py
+++ back/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://prometeodev.nlanube.mx"],  # Specify your frontend origin
+    allow_origins=["https://prometeodev.nlanube.mx"],  # Specify your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
